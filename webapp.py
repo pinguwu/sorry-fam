36c4043d503e9ee448c9f15e360a55b074c819ba
pinguwu <premixedcloud977@gmail.com>
2019-03-02 15:49:19 -0800

    lmao

---
+++ webapp.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask, url_for, render_template, request
+from flask import redirect
+from flask import session
+
+app = Flask(__name__)
+
+@app.route('/')
+def renderMain():
+    return render_template('home.html')
+
+
+if __name__=="__main__":
+    app.run(debug=False, port=12345)
